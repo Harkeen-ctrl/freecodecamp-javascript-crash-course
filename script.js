cb0ccd98acf177692cddd1f6f87a5b5ac3025824
Olapade <oluwapelumiakinpade@gmail.com>
2023-06-09 22:20:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//document.getElementById("count-el").innerText = 5
+
+/* let firstBatch = 10
+ let secondBatch = 4
+ 
+ let count = firstBatch + secondBatch
+ //document.getElementById("count-el").innerText = count
+
+// console.log(count)
+
+// let myAge = 22;
+ //console.log(myAge);*/
+
+/* let myAge = 22
+ let humanDogRatio = 7
+
+myDogAge = myAge * humanDogRatio
+console.log(myDogAge);*/
+function increment ()  {
+    console.log("The button was clicked")
+}
